737a39244ce36ac3ec99146ac03aea45fbdd220d
rtviii <rtkushner@gmail.com>
2021-12-17 14:31:39 -0800

    going thru the tutorial

---
+++ src/error.rs
@@ -0,0 +1,17 @@
+use thiserror::Error;
+
+use solana_program::program_error::ProgramError;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    #[error("InvalidInstruction")]
+    InvalidInstruction,
+    #[error("NotRentExempt")]
+    NotRentExempt
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod instruction;
+pub mod error;
+pub mod processor;
+pub mod entrypoint;
+pub mod state;
